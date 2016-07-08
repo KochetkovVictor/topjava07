37ca4cb6f0dd7a5651c44bfdf05c3b44b9def594
Kochetkov_V <che18@yandex.ru>
2016-07-08 11:03:45 +0300

    HW05 implement abstractservicetest

---
+++ src/test/java/ru/javawebinar/topjava/service/jpa/JapUserMealServiceTest.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjava.service.jpa;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.service.AbstractUserMealServiceTest;
+import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+
+@ActiveProfiles({Profiles.JPA})
+public class JapUserMealServiceTest extends AbstractUserMealServiceTest {
+
+}
