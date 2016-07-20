a7a227034a28e7373de0e722c63b61173af80119
Kochetkov_V <che18@yandex.ru>
2016-07-20 15:29:31 +0300

    HW07 preOptional

--- src/test/java/ru/javawebinar/topjava/web/AbstractControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/AbstractControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import ru.javawebinar.topjava.repository.JpaUtil;
+import ru.javawebinar.topjava.service.UserMealService;
 import ru.javawebinar.topjava.service.UserService;
 
 import javax.annotation.PostConstruct;
@@ -49,7 +50,8 @@ abstract public class AbstractControllerTest {
 
     @Autowired
     protected UserService userService;
-
+    @Autowired
+    protected UserMealService mealService;
     @Autowired
     private WebApplicationContext webApplicationContext;
 
--- src/test/java/ru/javawebinar/topjava/web/meal/UserMealRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/meal/UserMealRestControllerTest.java
@@ -1,41 +1,98 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.hibernate.mapping.Array;
 import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.util.TimeUtil;
+import ru.javawebinar.topjava.util.UserMealsUtil;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+import ru.javawebinar.topjava.web.json.JsonUtil;
 
-import static org.junit.Assert.*;
 
-/**
- * Created by Kochetkov_V on 20.07.2016.
- */
-public class UserMealRestControllerTest {
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+
+
+public class UserMealRestControllerTest extends AbstractControllerTest{
+
+    private static final String REST_URL = UserMealRestController.REST_URL + '/';
+
     @Test
-    public void getAll() throws Exception {
+    public void testGetAll() throws Exception {
+        mockMvc.perform(get(REST_URL))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MATCHER_WITH_EXCEED.contentListMatcher(UserMealsUtil.getWithExceeded(USER_MEALS,UserMealsUtil.DEFAULT_CALORIES_PER_DAY)));
+    }
+
+    @Test
+    public void tesGet() throws Exception {
+        mockMvc.perform(get(REST_URL+"100007"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MATCHER.contentMatcher(MEAL6));
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        mockMvc.perform(delete(REST_URL+"100007"))
+                .andDo(print())
+                .andExpect(status().isOk());
+                MATCHER.assertCollectionEquals(USER_MEALS1, mealService.getAll(100000));
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        UserMeal updated=MEAL1;
+        updated.setCalories(230);
+        updated.setDescription("Легкий завтрак");
+        mockMvc.perform(put(REST_URL + MEAL1_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated)))
+                .andExpect(status().isOk());
+       MATCHER.assertEquals(updated,mealService.get(MEAL1_ID,USER_ID));
+    }
+
+    @Test
+    public void testGetBetween() throws Exception {
+        mockMvc.perform(get(REST_URL+"between?startDateTime=2015-05-30T10:00&endDateTime=2015-05-30T14:00"))
+                .andDo(print())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+                MATCHER.assertCollectionEquals(
+                        Arrays.asList(MEAL2,MEAL1),
+                        mealService.getBetweenDateTimes(TimeUtil.parseLocalDateTime("2015-05-30 10:00"),
+                                                        TimeUtil.parseLocalDateTime("2015-05-30 14:00"),
+                                                        USER_ID));
 
     }
 
     @Test
-    public void get() throws Exception {
+    public void testCreateWithLocation() throws Exception {
+        UserMeal expected = new UserMeal(LocalDateTime.now(), "newPass", 1488);
+        ResultActions action = mockMvc.perform(post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(expected))).andExpect(status().isCreated());
 
-    }
+        UserMeal returned = MATCHER.fromJsonAction(action);
+        expected.setId(returned.getId());
 
-    @Test
-    public void delete() throws Exception {
-
-    }
-
-    @Test
-    public void update() throws Exception {
-
-    }
-
-    @Test
-    public void getBetween() throws Exception {
-
-    }
-
-    @Test
-    public void createWithLocation() throws Exception {
-
+        MATCHER.assertEquals(expected, returned);
+        MATCHER.assertCollectionEquals(Arrays.asList(expected,MEAL6,MEAL5,MEAL4,MEAL3,MEAL2,MEAL1), mealService.getAll(USER_ID));
     }
 
 }
